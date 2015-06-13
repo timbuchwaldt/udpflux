65d1e9615fc6d339fa02af860b8df364177f551d
Tim Buchwaldt <tim@buchwaldt.ws>
2015-06-13 23:51:13 +0200

    Actually send a message to a local test-socket

--- test/udpflux_test.exs
+++ test/udpflux_test.exs
@@ -2,7 +2,7 @@ defmodule UdpfluxTest do
   use ExUnit.Case
 
   setup do
-    {:ok, pid} = UDPFlux.Sender.start_link(%{ip: {127,0,0,1}, port: 4444})
+    {:ok, pid} = UDPFlux.Sender.start_link(%{ip: {127,0,0,1}, port: 2808})
     {:ok, pid: pid}
   end
 
@@ -16,12 +16,22 @@ defmodule UdpfluxTest do
     assert UDPFlux.DataPoint.dump(dp) == "foo,host=sample\\=bar value=\"deine\\ mudder\\,alda\""
   end
 
-  # that is rather hacky and I should really write a UDP acceptor that validates my message
   test "data sending doesn't crash instantly", %{pid: pid} do
+    # Create test-listener that delivers by sending a message to self
+    {:ok, socket_acceptor} = :gen_udp.open(2808)
+    :inet.setopts(socket_acceptor, [active: :once])
+    # we want messages as binaries (utf8-strings)
+    :inet.setopts(socket_acceptor, [mode: :binary])
+
     dp = %UDPFlux.DataPoint{name: "foo", tags: [host: "sample=bar"], fields: [value: "deine mudder,alda"]}
     UDPFlux.Sender.write_point(pid, dp)
-    # wait for the message to be evaluated
-    :timer.sleep(30)
+
+    # receive UDP datagram
+    d = receive do
+      {:udp, _port, _src_ip, _src_port, data} -> data
+    end
+
+    assert d == "foo,host=sample\\=bar value=\"deine\\ mudder\\,alda\""
     assert Process.alive?(pid)
   end
 end
