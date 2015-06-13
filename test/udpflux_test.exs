e065b768e9ec6abeb986594dd21e810cfd651e1a
Tim Buchwaldt <tim@buchwaldt.ws>
2015-06-13 19:47:51 +0200

    Clean up tests

--- test/udpflux_test.exs
+++ test/udpflux_test.exs
@@ -5,15 +5,6 @@ defmodule UdpfluxTest do
     {:ok, pid} = InfluxSender.start_link(%{ip: {127,0,0,1}, port: 4444})
     {:ok, pid: pid}
   end
-#
-#  test "Adding data to sender", %{pid: pid} do
-#    dp = %DataPoint{name: "foo", tags: [host: "sample"], fields: [value: 0.069]}
-#    InfluxSender.write_point(pid, dp)
-#    InfluxSender.write_point(pid, dp)
-#    InfluxSender.write_point(pid, dp)
-#    InfluxSender.write_point(pid, dp)
-#    :timer.sleep(100)
-#  end
 
   test "encoding packet" do
     dp = %DataPoint{name: "foo", tags: [host: "sample"], fields: [value: 0.069]}
