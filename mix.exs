3977d19a71c2ec4e1f7269343ac7b5494f8b44c3
Tim Buchwaldt <tim@buchwaldt.ws>
2015-06-14 00:00:44 +0200

    Tag 0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Udpflux.Mixfile do
   def project do
     [app: :udpflux,
      package: package,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
